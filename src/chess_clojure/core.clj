3b19265840aacf820d9b1b887102a29ac262b778
Simon Neutert <simonneutert@users.noreply.github.com>
2021-08-30 20:43:28 +0200

    sweet baby jesus, clojure :hearts:

--- src/chess_clojure/core.clj
+++ src/chess_clojure/core.clj
@@ -2,6 +2,32 @@
   (:gen-class))
 (require '[clojure.string :as str])
 
+(defn str->int
+  [s]
+  (-> s
+      str
+      Integer/parseInt))
+
+(defn keyword->str
+  [k]
+  (-> k
+      str
+      rest))
+
+(defn extract-x
+  [pos]
+  (-> pos
+      keyword->str
+      first
+      str->int))
+
+(defn extract-y
+  [pos]
+  (-> pos
+      keyword->str
+      last
+      str->int))
+
 (def board (atom {:round 0}))
 (def shadow-board (atom {}))
 (def board-killed-figurines (atom []))
@@ -18,55 +44,40 @@
 
 (defmulti make-keyword (fn [x] [(type x)]))
 (defmethod make-keyword [Figurine] [x]
-  (let [x (make-keyword [(:position-x x) (:position-y x)])]
-    (println x)
-    x))
+  (make-keyword [(:position-x x) (:position-y x)]))
 (defmethod make-keyword [clojure.lang.PersistentVector] [x]
   (-> x (str/join) (keyword)))
 
-(defn make-nil-hash
-  [val]
-  {(make-keyword val) nil})
+(defn make-nil-hash [val] {(make-keyword val) nil})
 
 (defn create-shadow-board
   []
-  (into
-   (sorted-map)
-   (for
-    [x '[a b c d e f g h]
-     y [1 2 3 4 5 6 7 8]]
-     (make-nil-hash [x y]))))
+  (into (sorted-map)
+        (for [x '[a b c d e f g h]
+              y [1 2 3 4 5 6 7 8]]
+          (make-nil-hash [x y]))))
 
 (defn create-board
   []
   (into
    (sorted-map)
-   (for [x (range 1 9) y (range 1 9)]
+   (for [x (range 1 9)
+         y (range 1 9)]
      (make-nil-hash [x y]))))
 
-(defn setup-board
-  []
+(defn setup-board []
   (swap! board merge (create-board)))
 
-(defn setup-shadow-board
-  []
+(defn setup-shadow-board []
   (swap! shadow-board merge (create-shadow-board)))
 
 (defn place-figurine-on-board
   [f]
   (swap! board merge {(make-keyword (deref f)) (deref f)}))
 
-(defn extract-x
-  [pos]
-  (Integer/parseInt (str (first (rest (str pos))))))
-
-(defn extract-y
-  [pos]
-  (Integer/parseInt (str (last (rest (str pos))))))
-
-(defn next-round
-  []
+(defn next-round []
   (swap! board merge {:round (inc (get @board :round))}))
+
 (defn update-board-positions
   [f new-position]
   ;; (show-move (deref f) new-position)
@@ -79,43 +90,40 @@
 
 (defn fight
   [f new-position enemy]
-  (println "Fighting:" enemy)
-  (swap! board-killed-figurines conj enemy)
-  (println "killed figurines:" @board-killed-figurines)
-  (update-board-positions f new-position))
+  (update-board-positions f new-position)
+  (swap! board-killed-figurines conj {:round (get @board :round) :enemy enemy}))
 
-
-(defn valid-field
+(defn valid-field-p
   [next-field]
-  (let
-   [x (extract-x next-field)
-    y (extract-y next-field)]
-    (println "" next-field "" x " " y)
+  (let [x (extract-x next-field)
+        y (extract-y next-field)]
     (and (> x 0) (> y 0) (< x 9) (< y 9))))
 
 (defn move-to-field
   [f next-field]
-  (if (valid-field next-field)
+  (if (valid-field-p next-field)
     (cond (= next-field 'error) 'error
           (nil? (get @board next-field)) (update-board-positions f next-field)
           (not (nil? (get @board next-field))) (fight f next-field (get @board next-field))
           :else 'error)
-    ()))
+    'error))
 
 (defn move-y
-  [f steps-x steps-y]
-  (let [x (:position-x (deref f)) y (+ steps-y (:position-y (deref f)))]
-    (println "xy" x y)
+  [f _ steps-y]
+  (let [x (:position-x (deref f))
+        y (+ steps-y (:position-y (deref f)))]
     (-> [x y] make-keyword)))
 
 (defn move-x
-  [f steps-x steps-y]
-  (let [x (+ steps-x (:position-x (deref f))) y (:position-y (deref f))]
+  [f steps-x _]
+  (let [x (+ steps-x (:position-x (deref f)))
+        y (:position-y (deref f))]
     (-> [x y] make-keyword)))
 
 (defn move-xy
   [f steps-x steps-y]
-  (let [x (+ steps-x (:position-x (deref f))) y (+ steps-y (:position-y (deref f)))]
+  (let [x (+ steps-x (:position-x (deref f)))
+        y (+ steps-y (:position-y (deref f)))]
     (-> [x y] make-keyword)))
 
 (defn determine-next-field
@@ -124,8 +132,7 @@
                          (and (= 'y way) (< steps-y 0)) (move-y f steps-x steps-y)
                          (and (= 'x way) (> steps-x 0)) (move-x f steps-x steps-y)
                          (and (= 'x way) (< steps-x 0)) (move-x f steps-x steps-y)
-                         (and (= 'xy way) (> steps-x 0) (> steps-y 0)) (move-xy f steps-x steps-y)
-                         (and (= 'xy way) (< steps-x 0) (< steps-y 0)) (make-keyword [(-  (:position-x (deref f)) steps-x steps-y) (- (:position-y (deref f)) steps-x steps-y)])
+                         (and (= 'xy way) (not= steps-x 0) (not= steps-y 0)) (move-xy f steps-x steps-y)
                          :else 'error)]
     (move-to-field f next-field)))
 
@@ -171,4 +178,5 @@
   (move black-pawn "up")
 
   (println "finished")
-  (println @board))
+  (println @board)
+  (println @board-killed-figurines))
